86d95c37e8da06c448b13b550bb56b0e2615685e
noaYoni <noa833@gmail.com>
2019-08-06 19:56:28 +0300

    a

--- movies/src/app/app-routing.module.ts
+++ movies/src/app/app-routing.module.ts
@@ -1,9 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-<<<<<<< HEAD
 import { HomeComponent } from './home/home.component';
-=======
->>>>>>> b8925708b7e13077e576d019ada6085320682ba6
 import { LoginComponent } from './login/login.component';
 
 
