98c3871d5f00119580e9deb450fac06ea895d5a4
noaYoni <noa833@gmail.com>
2019-08-13 22:16:50 +0300

    add multi col tag and finish the movies search

--- movies/src/app/components/search/pipes/movie-name.pipe.ts
+++ movies/src/app/components/search/pipes/movie-name.pipe.ts
@@ -1,19 +1,18 @@
-import { Pipe, PipeTransform } from '@angular/core';
-import * as AhoCorasick from 'AhoCorasick';
+import { Pipe, PipeTransform } from "@angular/core";
+import * as AhoCorasick from "AhoCorasick";
 
 @Pipe({
-  name: 'movieNameFilter'
+  name: "movieNameFilter"
 })
 export class MovieNamePipe implements PipeTransform {
-
   transform(movies: any, args?: any): any {
     if (args == undefined || args == "") {
       return movies;
     }
     let matches = [];
-    let ac = new AhoCorasick([args.toLowerCase()]);
+    let ac = new AhoCorasick([args.trim().toLowerCase()]);
     movies.forEach(movie => {
-      let results = ac.search(movie.title.toLowerCase());
+      let results = ac.search(movie.title.trim().toLowerCase());
       if (results.length > 0) {
         matches.push(movie);
       }
